dc49d1ebc0a893d09c5e000368b520242fa0d21f
Filip Napiorkowski <62057916+roguepanther@users.noreply.github.com>
2023-03-22 17:22:51 +0000

    basic todo add functionality added

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
  const userInput = document.querySelector('#taskInput')
+ const originalList = document.querySelector('.tasks')
 
  userInput.addEventListener('keydown', (e) => {
     if(e.keyCode === 13){
+        const listItem = document.createElement('li')
+        listItem.innerText = userInput.value
+        if(userInput.value.length === 0){
+            console.warn('cannot add empty task')
+        } else {
+            originalList.appendChild(listItem);
+        }
         console.log(userInput.value)
+
     } else {
         console.log('another key pressed')
     }
