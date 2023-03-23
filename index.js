1ad9b4d92a32d60383de76b7eb441b827dd6fba0
roguepanther <roguepanthernoob@gmail.com>
2023-03-23 18:06:56 +0000

    fixed local storage persistence

--- index.js
+++ index.js
@@ -14,7 +14,8 @@
         } else {
             // originalList.appendChild(listItem);
             // Add localstorage functionality to retrieve items and persist for user    
-            items.push(userInput.value)
+            // items.push(userInput.value)
+            pushToLocalStorage(userInput.value)
             createCard(userInput.value);
             // add item to localstorage array 
         }
@@ -90,8 +91,50 @@ function checkLocalStorage(){
                 createCard(item);
             })
             tempItems = items
-
         }   
+    } else {
+        localStorage.setItem('tasks', [])
+    }
+}
+
+// Function to push user task into local storage
+function pushToLocalStorage(localitem){
+    // grab the current values from the local storage and convert to array 
+    let currentLocal = JSON.parse(localStorage.getItem('tasks'))
+    //verify that the task entry exists in local storage
+    if(localStorage.getItem('tasks') != '' && localStorage.getItem('tasks') != 'null' ){
+        if("tasks" in localStorage){
+            // add the value that was added by user to the current array
+            console.log(currentLocal)
+            currentLocal.push(localitem)
+            // convert the array back into string 
+            tempLocal = JSON.stringify(currentLocal)
+            // add the temp local array into the localStorage 
+            localStorage.setItem('tasks', tempLocal)
+            console.log('item has been added to local storage successfully')
+        }   
+    } else {
+        // tasks entry does not exist in local storage 
+        console.log('tasks do not exist in local storage ')
+    }
+   
+}
+
+// Function to remove item from local storage
+function removeFromLocalStorage(localitem){
+    // grab the current values from the local storage and convert to array 
+    let currentLocal = JSON.parse(localStorage.getItem('tasks'))
+    //verify that the task entry exists in local storage
+    if(localStorage.getItem('tasks') != '' && localStorage.getItem('tasks') != 'null' ){
+        if("tasks" in localStorage){
+            var index = currentLocal.indexOf(JSON.stringify(localitem))
+            currentLocal = currentLocal.filter(v => v !== localitem); 
+            // replace the existing entries from local storage with the ones returned from filter
+            localStorage.setItem('tasks',JSON.stringify(currentLocal))
+        }   
+    } else {
+        // tasks entry does not exist in local storage 
+        console.log('tasks do not exist in local storage ')
     }
 }
 
@@ -101,4 +144,3 @@ window.onload = function(e) {
 }
 
 
- 
